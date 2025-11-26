a5e8f7ac0264ad78f37c479c936f7ac3d8186297
Shohruh223 <shohruh.abd2223@gmail.com>
2025-11-26 11:45:15 +0500

    add new commit

--- root/urls.py
+++ root/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from app.views import IndexView, AboutView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name="index"),
+    path('about/', AboutView.as_view(), name="about")
 ]
